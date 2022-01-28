ef06f4b313db8a6c12a5c26894260c68bc41f200
Sage Smith <sage@limitlessinteractive.com>
2022-01-27 19:25:14 -0600

    reduced hacknet purchases

--- phoenix.js
+++ phoenix.js
@@ -78,8 +78,8 @@ function determineResourceAllocation(servers, player) {
             {
                 cls: gs.msHnet,
                 compareFns: [
-                    (player.hacking.level < (200 * (1 / player.hnet.multipliers.purchaseCost))),
-                    (player.playtime.sinceAug < 30*60*1000)
+                    (player.hacking.level < (100 * (1 / player.hnet.multipliers.purchaseCost))),
+                    (player.playtime.sinceAug < 4*60*1000)
                 ]
             },
             {
