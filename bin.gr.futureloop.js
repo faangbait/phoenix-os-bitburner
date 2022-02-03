55cee4d040bd0c8cb35c3f8f2c8f518a2779f9c3
Inix <jmbise@gmail.com>
2022-02-02 19:40:53 -0600

    Minor Formatting
    Add space between "against" and "target".

--- bin.gr.futureloop.js
+++ bin.gr.futureloop.js
@@ -17,7 +17,7 @@
      let hackTime = ns.getHackTime(target);                          // 1000         // it works
      let runtime =  hackTime * hackTime_modifier;                    // 3200         // 
      let looptime = Math.max(hackTime * 4.2, 100);                   // 4200         // 
-     ns.tprint("Grow finished against", target, " at ", new Date().getSeconds(), ".",new Date().getMilliseconds());
+     ns.tprint("Grow finished against ", target, " at ", new Date().getSeconds(), ".",new Date().getMilliseconds());
                                                                           
      while (true) {                                                  // 852900       // 
          let curtime = new Date().valueOf();                         // 848700       // 
